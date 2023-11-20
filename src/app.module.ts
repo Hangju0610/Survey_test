c33495c74a4dd85414a75518b7edd726556e714a
hangju0610 <phj950610@naver.com>
2023-11-20 18:17:56 +0900

    Feat: Add GraphQLModule GraphQL 초기 설정 진행

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // GraphQL을 사용하기 위한 초기 설정
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      // 서버는 apollo 서버를 사용
+      driver: ApolloDriver,
+      playground: true,
+      // 스키마자동 생성 진행
+      // schema를 Entity나 DTO를 통해 자동으로 읽어, gql 파일 생성
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
