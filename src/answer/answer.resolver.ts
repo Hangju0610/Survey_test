72f64d18177b2de457170f03aa18358db1dd8b2b
hangju0610 <phj950610@naver.com>
2023-11-23 11:54:53 +0900

    Feat: Add updateAnswer updateAnswer 기능 추가

--- src/answer/answer.resolver.ts
+++ src/answer/answer.resolver.ts
@@ -8,12 +8,16 @@ export class AnswerResolver {
   constructor(private answerService: AnswerService) {}
 
   @Mutation(() => Answer)
-  async postAnswer(@Args('input') answerData: InputAnswer) {
+  async createAnswer(@Args('input') answerData: InputAnswer) {
     // 문항 개수와 답변 개수 일치여부 확인
     if (answerData.questionId.length !== answerData.answer.length)
       throw new BadRequestException('문항 수와 답변 수가 일치하지 않습니다.');
-    if (!answerData.id) {
-      return this.answerService.createAnswer(answerData);
-    }
+    return this.answerService.createAnswer(answerData);
+  }
+  @Mutation(() => Answer)
+  async updateAnswer(@Args('input') answerData: InputAnswer) {
+    if (answerData.questionId.length !== answerData.answer.length)
+      throw new BadRequestException('문항 수와 답변 수가 일치하지 않습니다.');
+    return this.answerService.updateAnswer(answerData);
   }
 }
--- src/answer/answer.service.ts
+++ src/answer/answer.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 import { AnswerEntity } from 'src/database/entities/answer.entity';
 import { QuestionEntity } from 'src/database/entities/question.entity';
 import { InputAnswer } from 'src/schema/answer.schema';
@@ -14,6 +15,7 @@ export class AnswerService {
     private readonly questionRepository: Repository<QuestionEntity>,
   ) {}
 
+  // 답변 생성
   async createAnswer(answerData: InputAnswer) {
     // 콜백 함수 비동기 처리 진행
     const score = await this.measureScore(
@@ -36,6 +38,66 @@ export class AnswerService {
     });
 
     return this.answerRepository.save(newAnswer);
+  }
+
+  // 답변 수정
+  async updateAnswer(answerData: InputAnswer) {
+    const { id, surveyId, user, questionId, answer } = answerData;
+    // 과거와 답변이 같은지, 같지 않은지 확인하는 작업 진행
+    // 1. 전 답변을 먼저 찾는다.
+    const oldAnswer = await this.answerRepository.findOneBy({
+      id,
+    });
+    let { score, totalScore } = oldAnswer;
+    // 변경할 답변의 문항 Id
+    const changeQuestionId: number[] = [];
+    // 변경된 답변
+    const changeAnswer: number[] = [];
+    // 변경할 점수의 index
+    const changeIndex: number[] = [];
+
+    // 2. 배열을 순회하며 새로운 답변과 과거 답변이 일치하는지 확인한다.
+    oldAnswer.answer.forEach((v, idx) => {
+      // 일치하지 않는다면 변경 진행
+      if (v !== answer[idx]) {
+        // 변경할 내역의 문항 Id, 답변 내역, 순서를 넣는다.
+        changeQuestionId.push(questionId[idx]);
+        changeAnswer.push(answer[idx]);
+        changeIndex.push(idx);
+      }
+    });
+    // 3. 변경할 답변의 문항이 있다면
+    if (changeQuestionId.length !== 0) {
+      // 변경할 문항만 점수 확인 진행
+      const changeScore = await this.measureScore(
+        changeQuestionId,
+        changeAnswer,
+        surveyId,
+      );
+      // 변경한 내역을 반복문을 돌려 넣어둔다.
+      changeIndex.forEach((v, idx) => {
+        score[v] = changeScore[idx];
+      });
+      totalScore = score.reduce((acc, cur) => {
+        return acc + cur;
+      }, 0);
+    } else {
+      throw new BadRequestException('변경 사항이 없습니다.');
+    }
+    // 4. update 진행
+    await this.answerRepository.update(
+      {
+        id,
+      },
+      {
+        user,
+        answer,
+        score,
+        totalScore,
+      },
+    );
+    const newAnswer = this.answerRepository.findOneBy({ id });
+    return newAnswer;
   }
 
   // 점수 측정 메서드
