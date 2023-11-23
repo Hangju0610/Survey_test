1b93ab1d4ae7c696f5cbea652ed28db32e2a86c8
hangju0610 <phj950610@naver.com>
2023-11-23 18:45:50 +0900

    Refactor: survey API Refactoring survey API를 리펙토링 진행 Common schema 작성 삭제 완료 시 응답을 반환해야 하기 때문

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SurveyEntity } from 'src/database/entities/survey.entity';
-import { InputSurvey } from 'src/schema/survey.schema';
+import { CreateSurvey, UpdateSurvey } from 'src/schema/survey.schema';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -21,17 +21,27 @@ export class SurveyService {
     return survey;
   }
 
-  async postSurvey(data: InputSurvey) {
-    // Save method는 해당하는 Id값이 있을 경우 데이터 수정을 하며,
-    // 해당하는 데이터가 없는 경우 INSERT를 진행한다.
-    const survey = await this.surveyRepository.save(data);
-    return survey;
+  async createSurvey(data: CreateSurvey) {
+    //save 메서드를 통해 insert 진행
+    return await this.surveyRepository.save(data);
+  }
+
+  async updateSurvey(data: UpdateSurvey) {
+    // 아이디 조회 진행
+    // save 메서드를 통해 데이터 수정
+    const result = await this.surveyRepository.update(
+      { id: data.id },
+      { title: data.title, description: data.description },
+    );
+    if (result.affected == 0)
+      throw new BadRequestException('설문지를 찾을 수 없습니다.');
+    return await this.getSurvey(data.id);
   }
 
   async deleteSurvey(id: number) {
     const result = await this.surveyRepository.delete(id);
     if (result.affected == 0)
       throw new BadRequestException('설문지를 찾을 수 없습니다.');
-    return { success: true };
+    return { success: true, message: '설문지 삭제 완료' };
   }
 }
